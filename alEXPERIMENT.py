003ac04239688868994f130420865b1c03d3b259
Din Mamma <alexander.gustavsson@skola.taby.se>
2023-11-15 11:48:19 +0100

    Start of skeleton, literally 3 lines lmao

---
+++ alEXPERIMENT.py
@@ -0,0 +1,20 @@
+class Description:
+    def __init__(self) -> None:
+        
+
+
+while (closeCommand == False):
+    command = input("INPUT  ----->>>  ")
+
+
+
+"""
+- Game name error
+- Description (Area / Monster / whatev)
+- 
+
+
+
+"""
+
+###
