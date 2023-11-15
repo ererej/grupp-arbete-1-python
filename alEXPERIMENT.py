02f4f9ea9a3bda4ceceef8af3a1781b675593235
Din Mamma <alexander.gustavsson@skola.taby.se>
2023-11-15 13:39:42 +0100

    Added an enumerator to store command values as well as some lines of code to repeat the main loop while the player did not enter the close command.

--- alEXPERIMENT.py
+++ alEXPERIMENT.py
@@ -1,11 +1,40 @@
-class Description:
-    def __init__(self) -> None:
+import os
+from enum import Enum
+
+#class PlayerInventory:
+    
+
+#class 
+
+
+
+class GameplayDescription:
+    def __init__(self):
+        print("\n Welcome to Farts and Boogies, your new favourite RPG game...")
+        self.currentText = ""
+
+    def NewDescription():
+        os.system('cls')
+
+
+
+class command(Enum):
+    CLOSE_COMMAND = "c"
+    IDLE = "e"
+
+
+# -----------------------------------------------------------------------------------------------
+
+LatestComm = command.IDLE
+def MAIN_LOOP():
+    LatestComm = command.IDLE
+    while (LatestComm != command.CLOSE_COMMAND):
+        while ((LatestComm in command._value2member_map_) == False):
+            LatestComm = input("  INPUT ------>>>  ")
+
         
 
-
-while (closeCommand == False):
-    command = input("INPUT  ----->>>  ")
-
+# -----------------------------------------------------------------------------------------------
 
 
 """
@@ -18,3 +47,5 @@ while (closeCommand == False):
 """
 
 ###
+
+MAIN_LOOP()
