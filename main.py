bd8b47dafe975b45a6683906c694923942e5a9e0
Ererej <erikeliassonjohansson@gmail.com>
2023-11-20 12:36:23 +0100

    fixade så att vi kan dectectera keypress

--- main.py
+++ main.py
@@ -13,7 +13,21 @@
 #Haha! Lite text till och med kanske i mitten
 
 
+import keyboard
 
+print("""
+[1]difficulty
+[2]start
+[3]shut down
+""")
 
-while (False):
-    print("hi!")
+while (True):
+    key =keyboard.read_key()
+    
+    if key == "1":
+        print("difficulty är inte implementerat")
+    
+    if key == "2":
+        mainloop()
+    if key == "3":
+        print(0/0)
