a435b9787ace3b28dfff1ca93039865f9cf44fc4
Jonas Friberg <jonas.friberg@skola.taby.se>
2023-11-21 13:32:10 +0100

    Just to be able to pull

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-# YOOOOOOOOOOOOOOOO funkar detta
+
+# | Tre dörrar framför dig. Vilken väljer du?|
+# |
+# | <description av dörr ett> [1]|
+# | 
+# | <description av dörr två> [2]|
+# |
+# | <description av dörr tre> [3]|
+# |
+# | |10 HP|11 STR|LVL 5|Inventory: [I]
 
 
-#Wow, such fun
-
-
-
-# please work/ joneas 
-
-
-#Wow, amazing!
-
-
-
-#Haha! Lite text till och med kanske i mitten
-
-
-
-
-while (False):
-    print("hi!")
+# |
+# | <Description: Du har kommit in. Vad som händer, presents a choice>
+# |
+# | {player action}
+# |
+# | <Description av effekterna, vilka rewards/punishments>
+# |
+# | {if-sats som checkar om combat är färdig.}
+# |
+# | <Går tillbaka till Main description>
+# |
+# |
+# |
