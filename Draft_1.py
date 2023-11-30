a041f7f3a64590a60bd82439fadc2ce8ede5617b
Ererej <erikeliassonjohansson@gmail.com>
2023-11-30 11:22:58 +0100

    löste ett fel jag gjorde pricis

--- Draft_1.py
+++ Draft_1.py
@@ -245,17 +245,17 @@ def PrintCharStats():
     charStats = (colored("Health: [" + '■'*(player.health) + ' '*(player.maxhealth-player.health) + "] ", "red") + colored(f"Strength: {player.strength} ", "yellow") + colored(f"Level: {roman.toRoman(player.level)} ", "green") + "\n")
     return(charStats + "reworking printing inventory")
     #lägger till ett långt sträck till stringen
-    for i in range(0, len(Inventory.items)):
-        charStats += "-"*(len(Inventory.items[i].name))
-    charStats += "-"*4*(6-len(Inventory.items))
+    for i in range(0, len(player.Inventory.items)):
+        charStats += "-"*(len(player.Inventory.items[i].name))
+    charStats += "-"*4*(6-len(player.Inventory.items))
     #lägger till item namen till stringen
-    for i in range(0, len(Inventory.items)):
-        charStats += Inventory.items[i].name + "|"
-    charStats += "    |"*(6-len(Inventory.items)) + "\n"
+    for i in range(0, len(player.Inventory.items)):
+        charStats += player.Inventory.items[i].name + "|"
+    charStats += "    |"*(6-len(player.Inventory.items)) + "\n"
     #lägger till ett till långt sträck till stringen
-    for i in range(0, len(Inventory.items)):
-        charStats += "-"*(len(Inventory.items[i].name))
-    charStats += "-"*4*(6-len(Inventory.items))
+    for i in range(0, len(player.Inventory.items)):
+        charStats += "-"*(len(player.Inventory.items[i].name))
+    charStats += "-"*4*(6-len(player.Inventory.items))
         
 
 player = Player(7, 10, 4)
