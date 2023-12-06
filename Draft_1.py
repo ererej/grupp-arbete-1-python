57d5dc42fc8bcc5ad30d6904e5818368235841e5
Jonas Friberg <jonas.friberg@skola.taby.se>
2023-12-06 16:10:31 +0100

    added treasure desc

--- Draft_1.py
+++ Draft_1.py
@@ -9,101 +9,56 @@ difficulty = 1
 screen1 = ""
 doorSet = [0, 0, 0]
 
-# 
-
-
 
 doorDescriptions = [[" Istället för ett vanligt handtag så har den första dörren en enorm tändsticka som handtag.", " Den andra dörren verkar långsamt bli mindre, som om den försvinner på samma sätt trä försvinner när det brinner.", " Den tredje dörren verkar vara gjord utav kol."], 
 [" En stor istapp hänger från den första dörrens handtag.", " Den andra dörren ser exakt ut som en snöflinga.", " Den tredje dörren verkar vara gjord av is."], 
 [" Den första dörren verkar ha en dolk istället för ett handtag, bäst att inte ta i den vassa delen."," Den andra dörren har formen av en upp-och-ner vänd sköld."," Den tredje dörrens utsida verkar se ut som man smälte ihop en stor mängd olika vapen."], 
 [" Den första dörren har en linjal istället för ett handtag."," Den andra dörren har en form som liknar grafen f(x)= -x**2 + 8x i intervallet 0 <= x <= 8."," Den tredje dörren ser ut att vara gjord linjaler, pennor och sudd."]]
 
+class Item():
+    def __init__(self, name, strength, health, type):
+
+        self.name = name
+        self.strength = strength
+        self.health = health
+        self.type = type
+
+    def CombatActive(self):
+
+
 
     
 
 class Inventory():
     def __init__(self):
-        self.items: list[Item] = []
-
-        self.items.append(Item("item name", 1, 3, [["asd"], ["asgas"], ["hgf"]], False, "Weapon", 3, ["Classy", "really cool"]))
+        pass
 
 
 class Player():
-    def __init__(self, health, maxhealth, strength):
+    def __init__(self, health, maxhealth, strenght):
         #innehåller all data om spelaren.
         self.level = 1
         self.exp = 0
         self.health = health
         self.maxhealth = maxhealth
-        self.strength = strength
+        self.strenght = strenght
         self.inventory = Inventory()
         self.weakness = [None, None]
 
-# the elements variable contains a list. 1: Take double damage of this type. 2: Take half damage of this type. 3: The monster deals this damage type.
+
 class Monster():
-    def __init__(self, monsterName, strength, health, elements: list, enterDesc, attackMoveDesc, deathDesc):
+    def __init__(self, monsterName, strength, health, enterDesc, attackMoveDesc, deathDesc):
     
         # Denna kod executar när monstret skapas. Här ska olika variabler som namn etc etc skapas, och stats slumpmässigt väljas.
         self.name = monsterName
 
         self.strength = RND.randint(strength * 0.7, strength * 1.3) * difficulty
         self.health = RND.randint(health * 0.7, health * 1.3) * difficulty
-        self.elements = elements
 
 
         self.attackMoveDesc = attackMoveDesc
         self.deathDesc = deathDesc
         self.enterDesc = enterDesc
-
-
-class Item():
-    def __init__(self, name, strength, health, element, consumable: bool, itemType, power, boostTypes: list):
-
-        self.name = name
-
-# These are stats given to the player who holds the item.
-        self.strength = strength
-        self.health = health
-
-# These are values critical to defining the functionality of the item. Element does not matter for pots.
-        self.element = element
-        self.consumable = consumable
-        self.itemType = itemType
-
-# Stats important for the functionality of specific items.
-        self.power = power
-        self.boostTypes = boostTypes
-
-    def ItemPickup(self):
-        player.health += self.health
-        player.maxhealth += self.health
-        player.strength += self.strength
-
-    def ItemDrop(self):
-        
-        player.maxhealth -= self.health
-        player.strength -= self.strength
-
-        player.inventory.items.remove(self)
-
-
-    def CombatActive(self, monster: Monster):
-
-        
-        if self.consumable == True:
-            self.ItemDrop()
-
-        # Weapons deal (player strength * damageMultiplier) damage of the item's type. Monsters can 
-        if self.itemType == "weapon":
-            monster 
-        
-        if self.itemType == "pot":
-            return ["pot", self.power]
-
-        if self.itemType == "boost":
-            return ["boost", ]
-
-    
 
 # This dictionary contains all data on different monster types. They are sorted into different groups. Group 0: fire. Group 1: ice. Group 2: Knighs/weaponry. 3: lärare
 
@@ -112,11 +67,10 @@ class Item():
 # Descriptions ska vara om entry i rummet, när monstret attakerar, när monstret dör
 
 encounterDictionary = {
-    0: [[''], [''], ["THE FIRE SLIME", 1, 6, "En slemmig, sfärisk varelse som dessutom brinner står framför dig!", "Monstret hoppar in i dig! Lyckligtvis så skadar inte dens kropp dig. Dock gör lågorna det.", "Lågorna på monstret slocknar, och det stelnar till och blir orörligt."], [''], ['dragon']],
-    1: [[''], [''], ["THE MAD SNOWMAN", 4, 3, "En snögubbe står framför dig! Han verkar dock inte glad att se dig.", "Snögubben kastar en snöboll på dig! Det skadar dig inte, men dock så gör kniven han kör in i din arm det.", "Snögubbens huvud och faller till marken, och ingen mer rörelse händer."], 
-        ["THE FROZEN SPIRIT"], ["THE GLACIER GOLEM"]],
-    2: [[''], [''], ["THE RIDER IN THE DARK"], [""], ["THE THOUSAND-PIERCED BEAR"]],
-    3: [[''], [''], ["JESPER ENGELMARK"], ["ANNIKA WESTIN"], ["MARTIN LOMAN"]]
+    0: [[''], [''], ['fire_slime', 3, 6, "En slemmig, sfärisk varelse som dessutom brinner står framför dig!", "Monstret hoppar in i dig! Lyckligtvis så skadar inte dens kropp dig. Dock gör lågorna det.", "Lågorna på monstret slocknar, och det stelnar till och blir orörligt."], [''], ['dragon']],
+    1: [[''], [''], ['mad_snowman', 4, 3, "En snögubbe står framför dig! Han verkar inte vara glad att se dig.", "Snögubben kastar en snöboll på dig! Det skadar dig inte, men dock så gör kniven han kör in i din arm det.", "Snögubbens huvud och faller till marken, och ingen mer rörelse händer."], ['ice_spirit'], ['frozen_golem']],
+    2: [[''], [''], ['ancient_soldier'], ['WWI_soldier'], ['']],
+    3: [[''], [''], ['Jesper Engelmark'], ['Annika Westin'], ['Martin Loman']]
 }
 
 
@@ -139,7 +93,6 @@ def Enter():
     difficultyIndex = 1
 
     while(True):
-
         os.system('cls')
 
         currentDifficulty: list = difficultyMap[difficultyIndex]
@@ -167,13 +120,7 @@ def Enter():
 
 def Combat(element):
     os.system('cls')
-
-    # Grams stats for monster. Randomized monster level. Max ceil scales as percentage as player level increases. lvl 10 is max lvl as of writing.
-
-    try:
-        MStats : list = list(encounterDictionary[element])[RND.randint(2, math.floor((player.level / 10) * (len(encounterDictionary[element]) - 2)))]
-    except:
-        MStats: list = list(encounterDictionary[element])[2]
+    MStats : list = list(encounterDictionary[element])[RND.randint(2, 2 + math.floor(player.level / 3))]
 
     encounteredMonster = Monster(MStats[0], MStats[1], MStats[2], MStats[3], MStats[4], MStats[5], MStats[6])
 
@@ -184,6 +131,7 @@ def Combat(element):
         while key not in ['1','2','3','4','5','6'] and int(key) > len(player.inventory):
             key = Input()
 
+        
 
 
 
@@ -231,13 +179,13 @@ def Main():
 
         if key in ['1', '2', '3']:
             
-            encounterStyle = RND.randint(1, 5)
+            encounterLevel = RND.randint(1, 5)
 
-            if(encounterStyle == 1):
+            if(encounterLevel == 1):
 
                 Treasure(doorSet[int(key)-1])
 
-            elif(encounterStyle == 2):
+            elif(encounterLevel == 2):
 
                 Trap(doorSet[int(key)-1])
 
@@ -250,28 +198,8 @@ def Main():
         
 
 def PrintCharStats():
-    charStats = (colored("Health: [" + '■'*(player.health) + ' '*(player.maxhealth-player.health) + "] ", "red") + colored(f"Strength: {player.strength} ", "yellow") + colored(f"Level: {roman.toRoman(player.level)} ", "green") + "\n")
+    return (colored("Health: [" + '■'*(player.health) + ' '*(player.maxhealth-player.health) + "] ", "red") + colored(f"Strength: {player.strenght} ", "yellow") + colored(f"Level: {roman.toRoman(player.level)} ", "green") + "\n" + "-"*31 + "\n|inventory preview place holder" + "|\n" + "-"*31 + "\n")
 
-    #return(charStats + "reworking printing inventory")
-    #lägger till ett långt sträck till stringen
-    for i in range(0, len(player.inventory.items)):
-        charStats += "" + "─"*(len(player.inventory.items[i].name)+1)
-
-# Vad ska den här göra? ser till att vi altid har 6 rutor
-    charStats += "─" * (5 * (6 - len(player.inventory.items)) + 1) + "\n│" 
-
-    #lägger till item namen till stringen
-    for i in range(0, len(player.inventory.items)):
-        charStats += player.inventory.items[i].name + "│"
-
-        # Verkar inte heller ha någon poäng visuellt
-    charStats += "    │"*(6-len(player.inventory.items)) + "\n"
-    #lägger till ett till långt sträck till stringen
-    for i in range(0, len(player.inventory.items)):
-        charStats += "─"*(len(player.inventory.items[i].name)+1)
-    charStats += "─"*(5*(6-len(player.inventory.items))+1)
-    return charStats
-        
 
 player = Player(7, 10, 4)
 
--- main.py
+++ main.py
@@ -1,58 +1,31 @@
-import os
+
 import keyboard
-import time
 
 difficulty = 2
 
-def start_screen_printer(difficulty: int):
-    """printar start screenen :D"""
-    os.system("cls")
-    if difficulty == 1:
-        difficulty = "Easy"
-    elif difficulty == 2:
-        difficulty = "Normal"
-    else:
-        difficulty = "Hard"
-    print(f"""
-[1]difficulty [{difficulty}]
+print("""
+[1]difficulty [impossible]
 [2]start
-[3]keybinds
-[4]shut down
+[3]shut down
 """)
 
-start_screen_printer(difficulty)
+class Printer:
+    def __init__(self) -> None:
+        self.ENCdesc = ""
 
-ofset = 0
+
+
+
 
 while (True):
-    if ofset == 1:
-        ofset = 0
-        print()
-        continue
-    ofset = 1
     key = keyboard.read_key()
     if keyboard._Event == keyboard.KEY_DOWN:
-        print("sup")
+        continue
     if key == "1": #den faking dubbel klickar!!!!
-        difficulty +=1
-        if difficulty >= 4:
-            difficulty = 1
-        start_screen_printer(difficulty)
     
     elif key == "2":
-        #mainloop()
-        pass
+        mainloop()
     
     elif key == "3":
-        #os.system("cls")
-        print("""
-Keybinds:
-Enter the left door:    [Q]            
-Enter the middle door:  [W]
-Enter the right door:   [E]
-""")
- 
-    elif key == "4":
         print(0/0)
-
 
