5beb96fc550de863aafd76d783d36823b9014642
Din Mamma <alexander.gustavsson@skola.taby.se>
2023-12-06 15:55:58 +0100

    added UX text in combat func

--- PrintCharStats_Inventory.py
+++ PrintCharStats_Inventory.py
@@ -313,14 +313,14 @@ def Combat(element):
 
 
     while (encounteredMonster.health > 0 and player.health > 0):
-        print(encounteredMonster.enterDesc)
+        print(encounteredMonster.enterDesc + "\n"*2 + "Use an item in your inventory to combat the shit out of it!\n")
 
         key = Input()
         while key not in ['1','2','3','4','5','6'] or int(key) > len(player.inventory.items):
             key = Input()
         
         usedItem = player.inventory.items[int(key) - 1]
-        print(f"You used {usedItem.name}!")
+        print(f"You used {usedItem.name}!\n")
         usedItem.CombatActive(encounteredMonster)
 
 
@@ -346,7 +346,7 @@ def Treasure(element):
 
 def Trap(element):
     os.system('cls')
-    print(encounterDictionary[element][1][0])
+    print(encounterDictionary[element][0][0])
     damageTaken = RND.randint(1, 2)
     player.health -= damageTaken
     print(f"You took {damageTaken}damage")
