131b9fbf7ed53538ef75d9e9d192848cc43b51a3
Ererej <erikeliassonjohansson@gmail.com>
2023-12-07 14:24:37 +0100

    sync

--- PrintCharStats_Inventory.py
+++ PrintCharStats_Inventory.py
@@ -339,6 +339,11 @@ def Combat(element):
         key = Input()
         while key not in ['1','2','3','4','5','6'] or int(key) > len(player.inventory.items):
             key = Input()
+
+            if key == 'i':
+                PrintInventory()
+            elif key == 'r':
+                PrintHelpMenu()
         
         usedItem = player.inventory.items[int(key) - 1]
         usedItem.CombatActive(encounteredMonster)
@@ -365,6 +370,7 @@ def Treasure(element):
     foundItem = Item(ItemStats[0], ItemStats[1], ItemStats[2], ItemStats[3], ItemStats[4], ItemStats[5], ItemStats[6], ItemStats[7], ItemStats[8])
     player.inventory.PickUpItem(foundItem, element)
 
+    os.system('cls')
     print(encounterList[element][0][1] + "\n"*2 + PrintCharStats(False) + "Press any key to continue!")
     Input()
 
