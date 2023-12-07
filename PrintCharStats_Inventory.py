cd6454949c3558e6e0ea4b00e2d4575196e84e33
Jonas Friberg <jonas.friberg@skola.taby.se>
2023-12-07 13:57:35 +0100

    Bug report: fire res potion crashar

--- PrintCharStats_Inventory.py
+++ PrintCharStats_Inventory.py
@@ -548,6 +548,29 @@ Enter(difficultyIndex)
 # DET FINNS INGET VAPEN I INVENTORY VID SPELETS START JUST NU MAN KAN EJ ANFALLA JUST NU
 # Notes: Öka antalet health spelaren börjar med, det är för lite ||| Förlorar man spelet behöver man starta upp koden igen ||| the blade of infinite infernal power är: 1. OP 2. typ ett måste för att vinna just nu p.g.a balancing |||
 # Rapporterar buggar till folk som kan koden bättre: Jonas
+
+# kan inte använda fire res potion:
+
+
+#You used a fire resistance potion!
+
+#Traceback (most recent call last):
+#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 546, in <module>
+#    Enter(difficultyIndex)
+#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 311, in Enter   
+#    Main()
+#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 463, in Main    
+#    Combat(doorSet[int(key) - 1])
+#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 344, in Combat  
+#    usedItem.CombatActive(encounteredMonster)
+#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 158, in CombatActive
+#    player.elements[ELEMENT_RESISTANCE].append(self.resistancePotEffects[i])
+#                                               ~~~~~~~~~~~~~~~~~~~~~~~~~^^^
+#TypeError: list indices must be integers or slices, not str
+
+
+
+
 #1. Försöker använda "teacher item placeholder"
 # Traceback (most recent call last):
 # File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 513, in <module>
