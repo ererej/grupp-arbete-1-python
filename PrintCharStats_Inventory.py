58bba07874efb118dca4844ae1d69901764020eb
Ererej <erikeliassonjohansson@gmail.com>
2023-12-07 13:50:50 +0100

    fixade instruktioner

--- PrintCharStats_Inventory.py
+++ PrintCharStats_Inventory.py
@@ -330,20 +330,15 @@ def Combat(element):
         print(encounteredMonster.enterDesc + "\n"*2)
 
         if encounteredMonster.health == monsterStartHealth:
-            print("Use an item in your inventory to combat the sh*t out of that sorry sack of sob")
+            print("press the index of an item in your inventory to use it and combat the sh*t out of that sorry sack of sob")
         else:
             print("The enemy has sustained a total of " + str(monsterStartHealth - encounteredMonster.health) + " damage.")
+            print("press the index of an item in your inventory to use it and combat the sh*t out of that sorry sack of sob")
 
         print(PrintCharStats(True))
         key = Input()
         while key not in ['1','2','3','4','5','6'] or int(key) > len(player.inventory.items):
             key = Input()
-
-            if key == 'i':
-                PrintInventory()
-            elif key == 'r':
-                PrintHelpMenu()
-
         
         usedItem = player.inventory.items[int(key) - 1]
         usedItem.CombatActive(encounteredMonster)
@@ -369,8 +364,6 @@ def Treasure(element):
     ItemStats: list = list(itemList[element])[RND.randint(0, len(itemList[element])-1)]
     foundItem = Item(ItemStats[0], ItemStats[1], ItemStats[2], ItemStats[3], ItemStats[4], ItemStats[5], ItemStats[6], ItemStats[7], ItemStats[8])
     player.inventory.PickUpItem(foundItem, element)
-
-    os.system('cls')
 
     print(encounterList[element][0][1] + "\n"*2 + PrintCharStats(False) + "Press any key to continue!")
     Input()
@@ -659,30 +652,3 @@ Enter(difficultyIndex)
 #                                            ~~~~~~^^^
 #IndexError: list index out of range
 #
-# Försökte gå in i dörr 2
-#In the next room you see three doors
-
-#[1] Instead of having a regular doorhandle, the door has a lighter instead.
-
-#[2] The door has the shape of a snowflake.
-
-#[3] The door seems to have been made out weapons that were hastely melted down, since you can still tell the door was made out of weapon metal.
-
-
-
-#Health: [■■■■■■■■■■■■■■■■] Strength: 15.0 Level: VIII 
-#╷────────────────╷─────────────────╷───────────────────────────╷──────────────────────────────────────╷──────────────────────────╷─────────────────╷
-#| a wooden sword | Exam awnser key | the gauntlets of strength | the blade of infinite infernal power | a fire resistance potion | Exam awnser key |
-#╵────────────────╵─────────────────╵───────────────────────────╵──────────────────────────────────────╵──────────────────────────╵─────────────────╵
-
-#Traceback (most recent call last):
-#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 545, in <module>
-##    Enter(difficultyIndex)
-#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 311, in Enter
-#    Main()
-#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 471, in Main
-#    Combat(doorSet[int(key) - 1])
-#  File "c:\Pythonprogramm\grupp-arbete-1-python\PrintCharStats_Inventory.py", line 324, in Combat
-#    encounteredMonster = Monster(MStats[0], MStats[1], MStats[2], MStats[3], MStats[4], MStats[5], MStats[6])
-#                                            ~~~~~~^^^
-#IndexError: list index out of range
